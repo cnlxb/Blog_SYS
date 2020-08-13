1f797a8cbe85238dd207e6026b68e16884bd7614
cnlxb <940329965@qq.com>
2020-08-13 20:07:22 +0800

    20200813_V04

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,9 +1,11 @@
 from django import template
+from django.db.models.aggregates import Count
 from ..models import *
 register = template.Library()
 
 @register.inclusion_tag('inclusions/_categories.html', takes_context=True)
 def show_categories(context):
+    category_list = Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
     return {
-        'category_list': Category.objects.all()
+        'category_list': category_list,
     }
